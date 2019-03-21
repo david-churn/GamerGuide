3f893f4f726767cd1da94b0b9f7b3b799091cdc1
David Churn <dchurn@me.com>
2019-03-21 18:30:17 -0500

    lint change

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,10 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    'eslint:recommended'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
